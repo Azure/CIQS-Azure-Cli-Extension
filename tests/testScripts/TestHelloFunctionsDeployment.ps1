f52fda7d9e28f055e1e7660a3026ce7bfc44a063
Jacob Hill <t-jahill@microsoft.com>
2018-07-03 13:18:28 -0700

    Added TestHelloFunctionsDeployment.ps1

---
+++ tests/testScripts/TestHelloFunctionsDeployment.ps1
@@ -0,0 +1,32 @@
+# TestHelloFunctionsDeployment
+
+$deployment = az ciqs deployment create -t "ciqs-hellofunctions" -l "East US" -n "testcli02" | ConvertFrom-Json
+if($LASTEXITCODE -ne 0) {
+    exit 1
+}
+
+$deploymentStatus = az ciqs deployment wait-terminal-status --deployment-id $deployment.uniqueId --timeout 180000 | ConvertFrom-Json
+if($LASTEXITCODE -ne 0){
+    exit 2
+}
+if(-not $deploymentStatus.status -eq "actionRequired"){
+    exit 3
+}
+
+$paramJson = @'
+{"""Name""":"""Pikachu"""}
+'@
+
+az ciqs deployment send-params --deployment-id $deployment.uniqueId --parameters $paramJson
+if($LASTEXITCODE -ne 0){
+    exit 4
+}
+$deploymentStatus = az ciqs deployment wait-terminal-status --deployment-id $deployment.uniqueId --timeout 180000 | ConvertFrom-Json
+if($LASTEXITCODE -ne 0){
+    exit 5
+}
+if(-not $deploymentStatus.status -eq "ready"){
+    exit 6
+}
+
+az ciqs deployment delete --deployment-id $deployment.uniqueId
