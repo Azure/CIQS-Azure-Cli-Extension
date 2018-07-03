8901efab07aa65c5f3b632b3e7a261172355f2f5
Jacob Hill <t-jahill@microsoft.com>
2018-07-03 09:41:11 -0700

    Added check for powershell script.

--- tests/RunTests.ps1
+++ tests/RunTests.ps1
@@ -3,22 +3,26 @@
 # Each test will output Passed or Failed
 # A test will fail if it exits on non zero exit code
 
+$env:CIQS_CLI_TEST="Remote"
+
 $originalColor = $Host.UI.RawUI.ForegroundColor
 
 $numberOfTests = (Get-ChildItem ".\testScripts").count
 $numberOfTestsRun = 0
 
 Get-ChildItem ".\testScripts" | ForEach-Object {
-    Write-Progress -Activity "Running Tests" -Status "Test $numberOfTestsRun/$numberOfTests"
-    & $_.FullName *>$null
-    $numberOfTestsRun++
-    if ($LASTEXITCODE -ne 0){
-        $Host.UI.RawUI.ForegroundColor = "Red"
-        Write-Output ("Failed -- Code: " + $LASTEXITCODE + " -- " + $_.Name)
-    }
-    else {
-        $Host.UI.RawUI.ForegroundColor = "DarkGreen"
-        Write-Output ("Passed -- " + $_.Name)
+    Write-Progress -Activity "Running Tests" -Status "Test $_"
+    if($_.Name.endswith(".ps1")){
+        & $_.FullName *>$null
+        $numberOfTestsRun++
+        if ($LASTEXITCODE -ne 0){
+            $Host.UI.RawUI.ForegroundColor = "Red"
+            Write-Output ("Failed -- Code: " + $LASTEXITCODE + " -- " + $_.Name)
+        }
+        else {
+            $Host.UI.RawUI.ForegroundColor = "DarkGreen"
+            Write-Output ("Passed -- " + $_.Name)
+        }
     }
     $Host.UI.RawUI.ForegroundColor = $originalColor
 }
